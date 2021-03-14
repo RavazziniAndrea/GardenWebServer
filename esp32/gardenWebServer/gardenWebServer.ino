f0e90b2272c5aba537281c4ed15bb7fead1724e1
Andrea <ravazziniandrea@hotmail.it>
2021-03-14 23:36:08 +0100

    added esp32 directory

---
+++ esp32/gardenWebServer/gardenWebServer.ino
@@ -0,0 +1,17 @@
+//LIBRARIES------------
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+// Replace network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+float sensorRead=-1;
+
+void setup() {
+
+}
+
+void loop() {
+  //TODO: add the sensorRead   
+}
