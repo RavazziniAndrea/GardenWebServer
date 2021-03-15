e303c9db253d726a335288d25c6b3cad09c7a386
Andrea <ravazziniandrea@hotmail.it>
2021-03-15 15:12:47 +0100

    sensor tests

--- esp32/gardenWebServer/gardenWebServer.ino
+++ esp32/gardenWebServer/gardenWebServer.ino
@@ -2,16 +2,59 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
-// Replace network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+//DEFINITIONS
+#define s1 A0
+#define s2 A1
+#define s3 A2
 
-float sensorRead=-1;
+//VARIABLES
+float sensor1=-1;
+float sensor2=-1;
+float sensor3=-1;
+const char* ssid = "Urla e avrai il WiFi.";
+const char* password = "";
 
-void setup() {
-
+String sensorRead(){
+  //average of 100 samplings
+  for (int i = 0; i <= 100; i++) { 
+    senso1 = sensor1 + analogRead(s1); 
+    senso2 = sensor2 + analogRead(s2); 
+    senso3 = sensor3 + analogRead(s3); 
+    delay(1); 
+  } 
+  senso1 = sensor1/100.0; 
+  senso2 = sensor2/100.0; 
+  senso3 = sensor3/100.0; 
+  delay(30);
 }
 
-void loop() {
-  //TODO: add the sensorRead   
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+void setup() {
+  Serial.begin(115200);  
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  // server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  //   request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  // });
+
+  // Start server
+  server.begin();
+}
+
+void loop() { 
+
 }
